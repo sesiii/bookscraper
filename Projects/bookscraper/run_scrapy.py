9de662a6f03638f9973aacb6b9ccbebfbbf2e8f4
Dadi Sasank Kumar <csesesi123@gmail.com>
2024-03-09 14:43:35 +0530

    Commit

--- Projects/bookscraper/run_scrapy.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def run_scrapy():
-    subprocess.Popen(". ./env/bin/activate && scrapy crawl bookscraper", shell=True, executable="/bin/bash")
-
-if __name__ == "__main__":
-    run_scrapy()
